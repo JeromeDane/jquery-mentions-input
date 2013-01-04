30dab16b35ad1b10b3b6daaead4c48576b5ec959
Brennan McEachran <brennan@hitsend.ca>
2013-01-03 19:31:36 -0500

    TODO allow the plugin to copy the styles of the textarea

--- jquery.mentionsInput.js
+++ jquery.mentionsInput.js
@@ -109,9 +109,38 @@
 
     function initMentionsOverlay() {
       elmMentionsOverlay = $(settings.templates.mentionsOverlay());
+      //! TODO: Allow the plugin to copy the styles of textarea
+      //elmMentionsOverlay.css(copyElemntStyle(domInput));
       elmMentionsOverlay.prependTo(elmWrapperBox);
     }
-
+    
+    //From: http://upshots.org/?p=112
+    function copyElemntStyle($el){
+        var dom = $el;
+        var style;
+        var returns = {};
+        if(window.getComputedStyle){
+            var camelize = function(a,b){
+                return b.toUpperCase();
+            };
+            style = window.getComputedStyle(dom, null);
+            for(var i = 0, l = style.length; i < l; i++){
+                var prop = style[i];
+                var camel = prop.replace(/\-([a-z])/g, camelize);
+                var val = style.getPropertyValue(prop);
+                returns[camel] = val;
+            };
+            return returns;
+        };
+        if(style = dom.currentStyle){
+            for(var prop in style){
+                returns[prop] = style[prop];
+            };
+            return returns;
+        };
+        return returns;
+    }
+    
     function updateValues() {
       var syntaxMessage = getInputBoxValue();
 
@@ -356,7 +385,7 @@
 
         var elmListItem = $(settings.templates.autocompleteListItem({
           'id'      : utils.htmlEncode(item.id),
-          'display' : utils.htmlEncode([settings.display]),
+          'display' : utils.htmlEncode(item[settings.display]),
           'type'    : utils.htmlEncode(item.type),
           'content' : utils.highlightTerm(utils.htmlEncode((item.display ? item.display : item.name)), query)
         })).attr('data-uid', itemUid);
